f9080c4086468abc412220611b766187dce9c903
Sebastian Guzman Obando <sguzmano@mefia.com.co>
2022-05-27 17:16:29 -0500

    63. Tarea con inputs y modulos - udemy

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.intercafe';
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre:'Goku',
+      poder:15000
+    },
+    {
+      nombre:'Vegeta',
+      poder:7500
+    }];
+
+   nuevo: Personaje = {
+     nombre:'Maestro Roshi',
+     poder:15
+    }
+  
+
+
+}
